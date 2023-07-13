0cd967b755f54397220e38c3593ff9eb2f2e8074
Darwin Algarin <dalgarins@live.com>
2023-07-12 20:37:01 -0500

    Added base code.

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+import com.github.gradle.node.yarn.task.YarnTask
+
+plugins {
+    id("java")
+    id("com.github.node-gradle.node") version "5.0.0" // NodeJS support
+    id("org.jetbrains.kotlin.jvm") version "1.8.21"
+    id("org.jetbrains.intellij") version "1.13.3"
+}
+
+group = "co.anbora.labs"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2022.2.5")
+    type.set("IC") // Target IDE Platform
+
+    plugins.set(listOf(/* Plugin Dependencies */))
+}
+
+// Set the Nodejs language
+node {
+    version.set("18.16.0")
+    distBaseUrl.set("https://nodejs.org/dist")
+    download.set(true)
+    yarnWorkDir.set(file("${project.projectDir}/.cache/yarn"))
+    nodeProjectDir.set(file("${project.projectDir}/src/main/resources/webview"))
+}
+
+tasks.register<YarnTask>("buildYarn") {
+    dependsOn(tasks.yarn)
+    yarnCommand.set(listOf("run", "build"))
+    args.set(listOf(
+        "--out-dir", "${project.projectDir}/src/main/resources/webview/dist"
+    ))
+    inputs.dir("src/")
+}
+
+tasks.register<Delete>("cleanYarnModules") {
+    delete("${project.projectDir}/src/main/resources/webview/node_modules")
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("222")
+        untilBuild.set("232.*")
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
