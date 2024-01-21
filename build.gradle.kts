7f4ccbeb53fd2f1cc3be83543c416759f4897c38
Darwin Algarin <dalgarins@live.com>
2024-01-20 20:52:32 -0500

    upgrade sdk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
     id("java")
     id("com.github.node-gradle.node") version "5.0.0" // NodeJS support
     id("org.jetbrains.kotlin.jvm") version "1.9.0"
-    id("org.jetbrains.intellij") version "1.16.1"
+    id("org.jetbrains.intellij") version "1.17.0"
 }
 
 group = "co.anbora.labs"
-version = "1.2.1"
+version = "1.2.2"
 
 repositories {
     mavenCentral()
@@ -65,8 +65,8 @@ tasks {
     }
 
     patchPluginXml {
-        sinceBuild.set("233")
-        untilBuild.set("233.*")
+        sinceBuild.set("241")
+        untilBuild.set("241.*")
         changeNotes.set(file("src/main/html/change-notes.html").inputStream().readBytes().toString(Charsets.UTF_8))
         pluginDescription.set(file("src/main/html/description.html").inputStream().readBytes().toString(Charsets.UTF_8))
     }
