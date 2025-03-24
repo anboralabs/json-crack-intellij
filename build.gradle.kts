1035784d81fcd3d201c0c49a14da507749ac8b85
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2025-03-24 11:44:07 -0500

    upgrade to version 2.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("java")
     id("com.github.node-gradle.node") version "5.0.0" // NodeJS support
     id("org.jetbrains.kotlin.jvm") version "2.0.0"
-    id("org.jetbrains.intellij.platform") version "2.2.1"
+    id("org.jetbrains.intellij.platform") version "2.4.0"
 }
 
 group = properties("pluginGroup").get()
